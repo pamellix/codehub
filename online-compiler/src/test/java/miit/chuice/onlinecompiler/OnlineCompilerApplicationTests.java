1f764e93f91dd39a1fde2fac1e3295f6e5b43b83
chuice <zuevPavelWork@gmail.com>
2024-11-13 01:47:53 +0300

    init compiler module

---
+++ online-compiler/src/test/java/miit/chuice/onlinecompiler/OnlineCompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package miit.chuice.onlinecompiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCompilerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
